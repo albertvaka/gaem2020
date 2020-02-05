86dcb9bd7a4d9950a60e490e8a42e0fad415a1c4
Albert Vaca <albert.vaca@datadoghq.com>
2020-02-05 21:38:52 +0100

    Ooops

--- src/game_data.h
+++ src/game_data.h
@@ -11,7 +11,7 @@ namespace GameData
 	const int WINDOW_WIDTH = 1472*2;
 	const int WINDOW_HEIGHT = 1000*2;
 #else
-	const int GAME_ZOOM = 2;
+	const int GAME_ZOOM = 4;
 	const int WINDOW_WIDTH = 1472;
 	const int WINDOW_HEIGHT = 1000;
 #endif
