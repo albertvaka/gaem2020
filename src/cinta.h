8934cc974c3ba5cdcfccf02851ff08b201e717e3
Albert Vaca Cintora <albertvaka@gmail.com>
2020-02-05 11:48:54 +0100

    Menos merda

--- src/cinta.h
+++ src/cinta.h
@@ -11,36 +11,12 @@ struct Cinta : Entity, EntS<Cinta>
 	Cinta(vec position, EntityDirection direction) {
 		pos = position;
 		anim.Ensure(AnimationType::BELT_RIGHT);
-		state = EntityState::MOVING;
 		dir = direction;
-	}
-
-	void Draw(sf::Sprite& spr, sf::RenderTarget& window)
-	{
-
 	}
 
 	void Update(float dt)
 	{
 
 	}
-	void Draw(sf::VertexArray &vertexArray)
-	{
-		/*
-		float x = pos.x / 100.0f;
-		float y = pos.y / 100.0f;
-		sf::Vector2f posStartInSpritesheet(100.f, 100.f);
-		sf::Vector2f sizeSprite(16.f, 16.f);
-		//sf::Vector2f sizeSprite(64.f, 64.f);
-		sf::Vector2u scale(4, 4);
-		vertexArray.append(sf::Vertex(sf::Vector2f(x, y), sf::Vector2f(posStartInSpritesheet.x, posStartInSpritesheet.y)));
-		vertexArray.append(sf::Vertex(sf::Vector2f(x + sizeSprite.x*scale.x, y), sf::Vector2f(posStartInSpritesheet.x + sizeSprite.x, posStartInSpritesheet.y)));
-		vertexArray.append(sf::Vertex(sf::Vector2f(x + sizeSprite.x*scale.x, y + sizeSprite.y*scale.y), sf::Vector2f(posStartInSpritesheet.x + sizeSprite.x, posStartInSpritesheet.y + sizeSprite.y)));
-		vertexArray.append(sf::Vertex(sf::Vector2f(x, y + sizeSprite.y*scale.y), sf::Vector2f(posStartInSpritesheet.x, posStartInSpritesheet.y + sizeSprite.y)));
-
-		*/
-
-	}
-
 
 };
