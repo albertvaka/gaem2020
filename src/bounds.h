cbf204d21e3879305e5d76a14ffd1951236124af
Albert Vaca <albert.vaca@datadoghq.com>
2020-02-13 15:01:20 +0100

    Bounds from pos and size

--- src/bounds.h
+++ src/bounds.h
@@ -14,10 +14,11 @@ struct Bounds : public sf::Rect<float>
     explicit Bounds() : sf::Rect<float>(0,0,0,0) { }
     explicit Bounds(sf::Rect<float> r) : sf::Rect<float>(r.left, r.top, r.width, r.height) { }
     explicit Bounds(const vec& v) : sf::Rect<float>(0,0,v.x,v.y) { }
+    explicit Bounds(const vec& pos, vec size) : sf::Rect<float>(pos.x,pos.y,size.x,size.y) { }
     explicit Bounds(float x, float y, float w, float h) : sf::Rect<float>(x,y,w,h) { }
 
     //Expands arround the center by an amount
-    void Expand(float x, float y) 
+    void Expand(float x, float y)
 	{
         width += x;
         left -= x/2;
@@ -28,7 +29,7 @@ struct Bounds : public sf::Rect<float>
     void Expand(const vec& v) { Expand(v.x, v.y); }
 
     //Expands arround the center by a factor
-    Bounds operator*(float f) 
+    Bounds operator*(float f)
 	{
         vec center = Center();
         Bounds ret = *this;
@@ -38,23 +39,23 @@ struct Bounds : public sf::Rect<float>
         return ret;
     }
 
-    vec Center() const 
+    vec Center() const
 	{
 		return vec(left + width/2, top + height/2);
 	}
 
-    void SetCenter(float x, float y) 
+    void SetCenter(float x, float y)
 	{
         left = x - width/2;
         top = y - height/2;
     }
 
-    void SetCenter(const vec& center) 
+    void SetCenter(const vec& center)
 	{
         SetCenter(center.x, center.y);
     }
 
-	void Draw(sf::RenderTarget &rt, sf::Color color = sf::Color::Red, sf::Color fillColor = sf::Color::Transparent) 
+	void Draw(sf::RenderTarget &rt, sf::Color color = sf::Color::Red, sf::Color fillColor = sf::Color::Transparent)
 	{
 		sf::RectangleShape rs;
 
@@ -68,27 +69,27 @@ struct Bounds : public sf::Rect<float>
 		rt.draw(rs);
 	}
 
-    float Top() const 
+    float Top() const
 	{
         return top;
     }
 
-    float Bottom() const 
+    float Bottom() const
 	{
         return top + height;
     }
 
-    float Left() const 
+    float Left() const
 	{
         return left;
     }
 
-    float Right() const 
+    float Right() const
 	{
         return left + width;
     }
 
-	bool IsInside(vec point) const 
+	bool IsInside(vec point) const
 	{
         if (point.x < left) return false;
         if (point.x >= left+width) return false;
